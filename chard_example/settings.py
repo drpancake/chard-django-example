83773042f40d3921555096088435dbd846c6a438
James Potter <james.potter@gmail.com>
2022-09-11 20:48:17 +0800

    added chard to requirements.txt + use new CHARD_* example settings

--- chard_example/settings.py
+++ chard_example/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+CHARD_MAX_CONCURRENT_TASKS = 10
+CHARD_TIMEOUT = 30
